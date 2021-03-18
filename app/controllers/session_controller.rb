95a295341b86ea0b5e3b0255604747be043629a5
smassarsky <seth.massarsky@gmail.com>
2021-03-18 15:44:47 -0400

    cookie change

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,7 @@ class SessionController < ApplicationController
     command = AuthenticateUser.call(params[:username], params[:password])
     if command.success?
       user = command.result[:user]
-      cookies.signed[:jwt] = {value: command.result[:token], httponly: true, same_site: :strict, expires: 1.hour.from_now}
+      cookies.signed[:jwt] = {value: command.result[:token], httponly: true, same_site: :lax, expires: 1.hour.from_now}
       render json: UserSerializer.new(user).to_serialized_json
     else
       render json: { error: "Unauthorized" }, status: :unauthorized
