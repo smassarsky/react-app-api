e05141e7c3c29a764ac451188ac41637486d3c88
smassarsky <seth.massarsky@gmail.com>
2021-03-18 15:35:29 -0400

    cors change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', '127.0.0.1', '172.17.69.57'
+    #origins 'http://localhost:3000', '127.0.0.1', '172.17.69.57'
+    origins 'intense-scrubland-75230.herokuapp.com'
 
     resource '*',
       headers: :any,
