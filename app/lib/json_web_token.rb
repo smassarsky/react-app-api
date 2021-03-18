36dda3a5a2692773b223aa8516614587e9b287f7
smassarsky <seth.massarsky@gmail.com>
2021-03-18 17:53:00 -0400

    deploy stuff

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      puts 'hi from jwt', Rails.application.secrets.secret_key_base
+      puts 'hi from jwt', ENV["RAILS_MASTER_KEY"]
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
  
